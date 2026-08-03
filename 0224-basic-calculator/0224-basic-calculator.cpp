2973478a00a018e638f7a6d22638e789937b82ea
Manashvi Sharma <ms7215@srmist.edu.in>
2026-08-03 12:09:36 +0530

    Time: 0 ms (100%), Space: 13.5 MB (51.73%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int calculate(string s) {
+        int n = s.length();
+        stack<int>st;
+        
+
+        long long number = 0;
+        long long result = 0;
+        int sign = 1;
+
+        for(int i = 0 ; i < n ; i++){
+            if(isdigit(s[i])){
+                number = number*10 + (s[i] - '0');
+            }
+            
+            else if(s[i] == '+'){
+                result += (number*sign);
+                number = 0;
+                sign = 1;
+            }
+            else if(s[i] == '-'){
+                result += (number*sign);
+                number = 0;
+                sign = -1;
+            }
+            else if(s[i] == '('){
+                st.push(result);
+                st.push(sign);
+                number = 0;
+                result = 0;
+                sign = 1;
+            }
+            else if(s[i] == ')'){
+                result += (number*sign);
+                number = 0;
+
+                int stack_sign = st.top(); st.pop();
+                int stack_result = st.top();st.pop();
+
+                result *= stack_sign;
+                result += stack_result; //result += last_result can be also used
+            }
+
+
+        }
+        result += (number*sign);
+        return result;
+    }
+};
