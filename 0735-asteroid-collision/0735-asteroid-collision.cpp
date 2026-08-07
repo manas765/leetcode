5a8859cb49c1cde8ae09fa602771ffd9df9c16fd
Manashvi Sharma <ms7215@srmist.edu.in>
2026-08-07 23:46:11 +0530

    Time: 0 ms (100%), Space: 21.4 MB (99.91%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+
+         vector<int>st;
+
+         for(int a : asteroids){
+            while(!st.empty() && a < 0 && st.back() > 0){ 
+               int sum = a + st.back();
+
+                if(sum > 0){
+                    a = 0;
+                } 
+                else if(sum < 0){
+                    st.pop_back();
+                }
+                else{
+                    a = 0;
+                    st.pop_back();
+                }
+
+         }
+            if(a != 0){ 
+                st.push_back(a);
+         } 
+
+         }
+    
+         return st;
+         } 
+    
+};
