92318ed29cd37a61c73ea638384e1e846f35b04e
Manashvi Sharma <ms7215@srmist.edu.in>
2026-09-14 23:34:14 +0530

    Time: 2 ms (13.15%), Space: 10.8 MB (42.44%) - LeetHub

---
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        unordered_map<int , int>mp;
+        for(int i = 0; i < nums.size(); i++){
+            mp[nums[i]]++;
+        }
+            int sum = 0;
+
+            for(auto i : mp){
+                int frequency = i.second;
+
+                if(frequency == 1){
+                    sum += i.first;
+                }
+
+            
+        }
+        return sum;
+        
+    }
+};
