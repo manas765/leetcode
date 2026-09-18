1c5bcf6df96b908ab67308f2640d788ead99045c
Manashvi Sharma <ms7215@srmist.edu.in>
2026-09-18 23:13:32 +0530

    Time: 0 ms (100%), Space: 9.9 MB (23.36%) - LeetHub

---
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        int n = nums.size();
+        int result = 0;
+
+        unordered_map<int , int>mp;
+
+        for(int &num : nums){
+            result += mp[num];
+            mp[num]++;
+        }
+        return result;
+        
+    }
+};
